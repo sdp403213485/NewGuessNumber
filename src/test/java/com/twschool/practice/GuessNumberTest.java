a83a2723b6de8432a58ade8b8c3e26d94c5b3f4f
sdp <403213485@qq.com>
2020-06-16 20:28:01 +0800

    should_return_0A0B_when_input_5678_given_1234

--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -30,4 +30,17 @@ public class GuessNumberTest {
         //then
         assertEquals(Expected,result);
     }
+
+    @Test
+    public void should_return_0A0B_when_input_5678_given_1234(){
+        //given
+        String userAnswer = "5678";
+        String gameAnsewr = "1234";
+        String Expected = "0A0B";
+        GuessNumber guessNumber = new GuessNumber();
+        //when
+        String result =guessNumber.answer(userAnswer,gameAnsewr);
+        //then
+        assertEquals(Expected,result);
+    }
 }
